e849e6c58525f2fea1bf9f9ddaa05c3aa1653be2
Vladimir Grichina <vgrichina@componentix.com>
2020-06-30 00:50:31 -0700

    Create project

---
+++ Tabless/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tabless
+//
+//  Created by Vladimir Grichina on 30.06.2020.
+//  Copyright © 2020 Organization. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
